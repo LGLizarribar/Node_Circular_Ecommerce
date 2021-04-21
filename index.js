6058e3c395213cd5ef54646f2efeded812e6adc5
Laura Garcia Lizarribar <laura.garcia.lizarribar@gmail.com>
2021-04-21 18:12:03 +0200

    CONFIG: add session, sass and method override

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
 require('dotenv').config();
 const express = require('express');
+const path = require('path');
+const passport = require('passport');
+const sassMiddleware = require('node-sass-middleware');
+const methodOverride = require('method-override');
+const session = require('express-session');
+const MongoStore = require('connect-mongo');
 const db = require('./db');
+
+const authRoutes = require('./routes/auth.routes');
 
 
 const app = express();
@@ -9,9 +17,43 @@ const PORT = process.env.PORT || 3000;
 
 db.connect();
 
+app.use(session({
+  secret: process.env.SESSION_SECRET,
+  resave: false,
+  saveUninitialized: false,
+  cookie: {
+    maxAge: 48 * 60 * 60 * 1000,
+  },
+  store: MongoStore.create({mongoUrl: db.DB_URL}),
+}));
+
+app.use(
+    sassMiddleware({
+      src: path.join(__dirname, 'public'),
+      dest: path.join(__dirname, 'public'),
+      debug: false,
+      outputStyle: 'compressed',
+    }),
+);
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'hbs');
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+
+app.use(methodOverride('_method'));
+
+
 app.use('/', (req, res) => {
   res.send('Hello world!');
 });
+app.use('/auth', authRoutes);
 
 
 app.use('*', (req, res, next) => {
