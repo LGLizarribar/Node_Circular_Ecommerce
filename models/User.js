5b756dfc8783e19ce85d5601cb5c581451cd6713
Laura Garcia Lizarribar <laura.garcia.lizarribar@gmail.com>
2021-04-21 17:00:38 +0200

    FEAT: add model User

---
+++ models/User.js
@@ -0,0 +1,21 @@
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema;
+
+const userSchema = new Schema({
+  name: {type: String, required: true},
+  surname: {type: String, required: true},
+  city: {type: String, required: true},
+  email: {type: String, required: true},
+  password: {type: String, required: true},
+  userImg: {type: String},
+  role: {type: String, enum: ['user', 'admin'], default: 'user'},
+},
+{
+  timestamps: true,
+},
+);
+
+const User = mongoose.model('User', userSchema);
+
+module.exports = User;
