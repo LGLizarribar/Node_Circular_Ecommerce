e61af3ca6cce9d252c586ce782085443d6e6926a
Laura Garcia Lizarribar <laura.garcia.lizarribar@gmail.com>
2021-04-21 17:07:39 +0200

    FEAT: add error management

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ require('dotenv').config();
 const express = require('express');
 const db = require('./db');
 
+
 const app = express();
 
 const PORT = process.env.PORT || 3000;
@@ -10,6 +11,18 @@ db.connect();
 
 app.use('/', (req, res) => {
   res.send('Hello world!');
+});
+
+
+app.use('*', (req, res, next) => {
+  const error = new Error('Route not found');
+  error.status = 404;
+  next(error);
+});
+
+app.use((error, req, res, next) => {
+  return res.status(error.status || 500)
+      .render('error', {error: error.message || 'Unexpected error'});
 });
 
 
